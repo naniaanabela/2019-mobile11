110107c59f93571234da3a3843b0aa0f6eca9be6
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2019-11-18 13:39:23 +0700

    menambahkan kode untuk mengatur layout preferences pada method onCreatePreferences

--- SettingsExample/app/src/main/java/id/ac/polinema/settingsexample/fragments/SettingsFragment.java
+++ SettingsExample/app/src/main/java/id/ac/polinema/settingsexample/fragments/SettingsFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import id.ac.polinema.settingsexample.R;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 }
